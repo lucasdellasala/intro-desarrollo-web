31e713bc09e87f0f328fe4b9ae0d46ccd03c1b6f
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2021-07-06 20:32:40 -0300

    No anda cors

--- proyectos/todo-list-api/index.js
+++ proyectos/todo-list-api/index.js
@@ -1,58 +1,86 @@
 import Express from 'express'
 import Bd from './bd.js'
-import bodyParser from 'body-parser'
+import cors from 'cors'
+
 
 //Settings
 const PORT = 5000
 const app = Express()
 
+app.use(cors())
 app.use(Express.json())
 
-app.get("/", (request, response)=>{
-    if(request.query.completed == "false"){
-        const bdFalse = Bd.filter((todo)=> todo.completed == false)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+app.get("/", (request, response) => {
+    if (request.query.completed == "false") {
+        const bdFalse = Bd.filter((todo) => todo.completed == false)
         response.send(bdFalse)
-    } else if(request.query.completed == "true"){
-        const bdTrue = Bd.filter((todo)=> todo.completed == true)
+    } else if (request.query.completed == "true") {
+        const bdTrue = Bd.filter((todo) => todo.completed == true)
         response.send(bdTrue)
     } else {
         response.send(Bd)
     }
 })
 
-app.get("/:id",(request, response)=>{
+app.get("/:id", (request, response) => {
     const idSolicitado = parseInt(request.params.id)
 
-    const todoSolicitado = Bd.find((todo)=>todo.id == idSolicitado)
-    if(!todoSolicitado){
+    const todoSolicitado = Bd.find((todo) => todo.id == idSolicitado)
+    if (!todoSolicitado) {
         response.status(404).json({
-            "status-code": 404,
+            "statusCode": 404,
             "message": "El id solicitado no existe"
         })
     }
     response.send(todoSolicitado)
 })
 
-app.post("/add", (request, response)=>{
+app.post("/add", (request, response) => {
     //Validamos campos obligatorios
-    if(request.body.title == "" || request.body.responsable == ""){
-        response.status(400).json({"message":"Debes ingresar obligatoriamente title y responsable."})
+
+    //Validamos campos obligatorios
+    function isEmpty(obj) {
+        return Object.keys(obj).length === 0;
+    }
+
+    console.log("BODY", request.body)
+
+    if (isEmpty(request.body)) {
+        response.status(400).json({ "message": "Debes enviar un body." })
+        return
+    }
+
+    if (request.body.title == "" || request.body.responsable == "") {
+        console.log("POST TITLE", request.body.title)
+        console.log("POST RESPONSABLE", request.body.responsable)
+
+        response.status(400).json({ "message": "Debes ingresar obligatoriamente title y responsable." })
+        return
     }
 
     let minIdPossible = 0
-    for(let i = 0; i< Bd.length; i++){
-        const comparator = i+1
-        if(comparator==Bd.length){
-            minIdPossible = Bd.length+1
+    for (let i = 0; i < Bd.length; i++) {
+        const comparator = i + 1
+        if (comparator == Bd.length) {
+            minIdPossible = Bd.length + 1
         }
-        if(comparator!=Bd[i].id){
+        if (comparator != Bd[i].id) {
             minIdPossible = comparator
             break
         }
     }
     const title = request.body.title
     const responsable = request.body.responsable
-    
+
     let deadline = request.body.deadline
 
     if (!deadline) {
@@ -70,60 +98,60 @@ app.post("/add", (request, response)=>{
 
     const urgency = !request.body.urgency ? "low" : request.body.urgency
     const completed = false
-
-
-    Bd.push({
+    const pushedObj = {
         "id": minIdPossible,
-        "title": title, 
+        "title": title,
         "responsable": responsable,
-        "deadline": deadline, 
-        "urgency": urgency, 
+        "deadline": deadline,
+        "urgency": urgency,
         "completed": completed
-    })
+    }
+    console.log({ pushedObj })
+    Bd.push(pushedObj)
 
-    response.status(201).json({"message":"La tarea fue creada satisfactoriamente."})
+    response.status(201).json({ "message": "La tarea fue creada satisfactoriamente." })
 })
 
-app.put("/update/:id",(request, response)=>{
+app.put("/update/:id", (request, response) => {
     const idSolicitado = parseInt(request.params.id)
-    
-    const todoSolicitado = Bd.find((todo)=>todo.id == idSolicitado)
 
-    if(!todoSolicitado){
+    const todoSolicitado = Bd.find((todo) => todo.id == idSolicitado)
+
+    if (!todoSolicitado) {
         response.status(404).json({
-            "status-code": 404,
+            "statusCode": 404,
             "message": "El id solicitado no existe"
         })
     }
 
     todoSolicitado.completed = true
 
-    console.log({todoSolicitado})
+    console.log({ todoSolicitado })
 
     const indexTodoSolicitado = Bd.indexOf(todoSolicitado)
     Bd.splice(indexTodoSolicitado, 1, todoSolicitado)
 
-    response.status(202).json({"message":"La tarea ha sido finalizada."})
+    response.status(202).json({ "message": "La tarea ha sido finalizada." })
 })
 
-app.delete("/:id",(request, response)=>{
+app.delete("/:id", (request, response) => {
     const idSolicitado = parseInt(request.params.id)
-    
-    const todoSolicitado = Bd.find((todo)=>todo.id == idSolicitado)
 
-    if(!todoSolicitado){
+    const todoSolicitado = Bd.find((todo) => todo.id == idSolicitado)
+
+    if (!todoSolicitado) {
         response.status(404).json({
-            "status-code": 404,
+            "statusCode": 404,
             "message": "El id solicitado no existe"
         })
     }
 
-    console.log({todoSolicitado})
+    console.log({ todoSolicitado })
 
     const indexTodoSolicitado = Bd.indexOf(todoSolicitado)
     Bd.splice(indexTodoSolicitado, 1)
 
-    response.status(202).json({"message":"La tarea ha sido eliminada."})
+    response.status(202).json({ "message": "La tarea ha sido eliminada." })
 })
 
-app.listen(PORT,()=>console.log("ESCUCHANDO EL PUERTO "+PORT))
+app.listen(PORT, () => console.log("ESCUCHANDO EL PUERTO " + PORT))
